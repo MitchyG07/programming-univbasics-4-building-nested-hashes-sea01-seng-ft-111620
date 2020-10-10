751a1db599fdc69ffd56da1db70d027b19faae48
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-10-10 00:54:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -42,7 +42,7 @@ def monopoly_with_fourth_tier
       },
       :names => {
         :reading_railroad => {},
-        :pennslyvania_railroad => {},
+        :pennsylvania_railroad => {},
         :b_and_o_railroad => {},
         :shortline_railroad => {}
       }
